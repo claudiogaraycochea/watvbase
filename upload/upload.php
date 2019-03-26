1a838c29a00ae8aaa652ec0a9c89c5d51002e495
Claudio Garaycochea <computadoraweb@gmail.com>
2019-03-26 17:16:00 -0300

    Upload fixed

--- upload/upload.php
+++ upload/upload.php
@@ -1,115 +1,42 @@
-<?php
+<?php 
+  header('Access-Control-Allow-Origin: *');
+  $method = $_SERVER['REQUEST_METHOD'];
+  if($method == "OPTIONS") {
+    die();
+  }
+  $tmp_name = $_FILES["image"]["tmp_name"];
+	$file_name = $_FILES["image"]["name"];
+  $file_type = $_FILES["image"]["type"];
+  $file_size = $_FILES["image"]["size"];
+  echo "_18";
+  /*echo "File Name: ".$file_name;
+  echo "File Type: ".$file_type;
+  echo "File Size: ".$file_size;*/
+  //print_r($_FILES["image"]);
 
-	define("NAMETHUMB", "/tmp/thumbtemp");
+/*  $image_size = getimagesize($tmp_name); 
+  print_r($image_size);*/
+  function isFormatAccepted($file_type){
+    if(($file_type=="image/png")||($file_type=="image/jpeg")){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 
-	function resize($img, $w, $h, $newfilename) {
-		
-		//Check if GD extension is loaded
-		if (!extension_loaded('gd') && !extension_loaded('gd2')) {
-			trigger_error("GD is not loaded", E_USER_WARNING);
-			return false;
-		}
-		
-		//Get Image size info
-		$imgInfo = getimagesize($img);
-		switch ($imgInfo[2]) {
-			case 1: $im = imagecreatefromgif($img); break;
-			case 2: $im = imagecreatefromjpeg($img);  break;
-			case 3: $im = imagecreatefrompng($img); break;
-			default:  trigger_error('Unsupported filetype!', E_USER_WARNING);  break;
-		}
-		
-		//If image dimension is smaller, do not resize
-		if ($imgInfo[0] <= $w && $imgInfo[1] <= $h) {
-			$nHeight = $imgInfo[1];
-			$nWidth = $imgInfo[0];
-		}else{
-			//yeah, resize it, but keep it proportional
-			if ($w/$imgInfo[0] > $h/$imgInfo[1]) {
-				$nWidth = $w;
-				$nHeight = $imgInfo[1]*($w/$imgInfo[0]);
-			}else{
-				$nWidth = $imgInfo[0]*($h/$imgInfo[1]);
-				$nHeight = $h;
-			}
-		}
-		$nWidth = round($nWidth);
-		$nHeight = round($nHeight);
-		
-		$newImg = imagecreatetruecolor($nWidth, $nHeight);
-		
-		/* Check if this image is PNG or GIF, then set if Transparent*/  
-		if(($imgInfo[2] == 1) OR ($imgInfo[2]==3)){
-			imagealphablending($newImg, false);
-			imagesavealpha($newImg,true);
-			$transparent = imagecolorallocatealpha($newImg, 255, 255, 255, 127);
-			imagefilledrectangle($newImg, 0, 0, $nWidth, $nHeight, $transparent);
-		}
-		imagecopyresampled($newImg, $im, 0, 0, 0, 0, $nWidth, $nHeight, $imgInfo[0], $imgInfo[1]);
-		
-		//Generate the file, and rename it to $newfilename
-		switch ($imgInfo[2]) {
-			case 1: imagegif($newImg,$newfilename); break;
-			case 2: imagejpeg($newImg,$newfilename);  break;
-			case 3: imagepng($newImg,$newfilename); break;
-			default:  trigger_error('Failed resize image!', E_USER_WARNING);  break;
-		}
-
-		return $newfilename;
-	}
-
-	function subir_imagen($original,$archivo_final,$anchura,$type){
-		$w=$anchura;
-		$h=$anchura;
-		$new_file=resize($original, $w, $h, $archivo_final);
-		copy($new_file,$archivo_final);
-		return true;
-	}
-
-	function upload_photo($original,$destino,$big,$type) {
-		subir_imagen($original,$destino,$big,$type);
-		return true;
-	}
-
-	function bytesToSize1024($bytes, $precision = 2) {
-		$unit = array('B','KB','MB');
-		return @round($bytes / pow(1024, ($i = floor(log($bytes, 1024)))), $precision).' '.$unit[$i];
-	}
-
-	$sFileName = $_FILES['file']['name'];
-	$sFileType = $_FILES['file']['type'];
-	$sFileSize = bytesToSize1024($_FILES['file']['size'], 1);
-					
-	$tmp_name = $_FILES["file"]["tmp_name"];
-	$path_parts = pathinfo($_FILES["file"]["name"]);
-	$extension = $path_parts['extension'];
-	$name = $_FILES["file"]["name"];
-	$size = $_FILES["file"]["size"];
-	$website_id = '1';
-	$hash=date('dMYHis');
-	$name_file = 'website_bg_'.$website_id.'_'.$hash.'.'.$extension;
-
-	$dirname = $website_id;  
-	$filename = "/upload/{$dirname}";  
-	if(($size<5000000)&&(($extension=='jpg')||($extension=='png'))){
-		if (!file_exists($filename)) {  
-			mkdir($filename, 0777);
-		}
-		if(move_uploaded_file($tmp_name, '/upload/'.$dirname.'/'.$name_file)){
-			$data['filename']=$name_file;
-			$data['filename_server']='https://modules.weband.tv/upload/'.$dirname.'/'.$name_file;
-			echo json_encode($data);
-
-			$original=$data['filename_server'];
-			$destino='/upload/'.$dirname.'/'.$name_file;
-			$big=300;
-			$type='jpg';
-			upload_photo($original,$destino,$big,$type);
-		}
-	}
-	else {
-		$data['filename']='';
-		echo json_encode($data);
-	}
+  if(($file_size<2000000)&&($file_size!='')&&(isFormatAccepted($file_type)==1)){
+    echo "File accepted!!!!";
+    $result= move_uploaded_file($_FILES["image"]["tmp_name"], 'upload/'.$file_name);
+    if($result['error']==0){
+      echo "Uploaded succefully";
+    }
+    else {
+      echo "Error upload";
+    }
+  }
+  else {
+    echo "ERROR";
+  }
 
 ?>
